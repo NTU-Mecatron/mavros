a6b28349f10ee5db6a0632a4d8ab12fdb2001780
GBaoBot <baobui226mh@gmail.com>
2025-09-09 08:19:55 +0800

    Simplified

--- mavros_extras/src/plugins/remap_setpoint_velocity.cpp
+++ mavros_extras/src/plugins/remap_setpoint_velocity.cpp
@@ -10,41 +10,37 @@
 #include "geometry_msgs/msg/twist.hpp"
 
 #include <mutex>
+#include <chrono>
 
-namespace mavros
-{
-namespace extra_plugins
-{
+namespace mavros {
+namespace extra_plugins {
+
 using namespace std::placeholders;
 using mavlink::common::MAV_FRAME;
 
-/**
- * @brief Custom Setpoint velocity plugin
- * @plugin custom_setpoint_velocity
- *
- * Send setpoint velocities to FCU controller via individual Float32 topics.
- */
-class RemapSetpointVelocityPlugin : public plugin::Plugin,
-  private plugin::SetPositionTargetLocalNEDMixin<RemapSetpointVelocityPlugin>
-{
+class RemapSetpointVelocityPlugin :
+  public plugin::Plugin,
+  private plugin::SetPositionTargetLocalNEDMixin<RemapSetpointVelocityPlugin> {
 public:
   explicit RemapSetpointVelocityPlugin(plugin::UASPtr uas_)
   : Plugin(uas_, "remap_setpoint_velocity"),
     reset_timeout_(1.0)
   {
     enable_node_watch_parameters();
+
     node_declare_and_watch_parameter(
-    "remap_setpoint_vel_reset_timeout_", 1.0, [&](const rclcpp::Parameter & p) {
-      reset_timeout_ = p.as_double();      
-    });
+      "remap_setpoint_vel_reset_timeout", 1.0,
+      [&](const rclcpp::Parameter & p) {
+        reset_timeout_ = p.as_double();
+      });
 
     auto sensor_qos = rclcpp::SensorDataQoS();
 
     twist_pub_ = node->create_publisher<geometry_msgs::msg::Twist>(
       "setpoint_velocity/cmd_vel_unstamped", 10);
-      
+
     vel_x_sub_ = node->create_subscription<std_msgs::msg::Float32>(
-      "setpoint_velocity/cmd_vel_unstamped/x", sensor_qos, 
+      "setpoint_velocity/cmd_vel_unstamped/x", sensor_qos,
       [this](const std_msgs::msg::Float32::SharedPtr msg) {
         std::lock_guard<std::mutex> lock(mutex_);
         twist_.linear.x = msg->data;
@@ -75,14 +71,11 @@ public:
         last_vel_r_time_ = node->now();
       });
 
+    using namespace std::chrono_literals;
     publish_timer_ = rclcpp::create_timer(
-      node, node->get_clock(), rclcpp::Duration::from_seconds(0.1),
-      [this]() {
-        std::lock_guard<std::mutex> lock(mutex_);
-        if (is_any_active()) {
-          twist_pub_->publish(twist_);
-        }
-      });
+      node, node->get_clock(),
+      rclcpp::Duration(100ms),
+      std::bind(&RemapSetpointVelocityPlugin::publisher_timer_cb, this));
 
     auto now = node->now();
     last_vel_x_time_ = now;
@@ -90,78 +83,60 @@ public:
     last_vel_z_time_ = now;
     last_vel_r_time_ = now;
 
-    // Initialize twist message
-    twist_.linear.x = 0.0;
-    twist_.linear.y = 0.0;
-    twist_.linear.z = 0.0;
-    twist_.angular.x = 0.0;
-    twist_.angular.y = 0.0;
-    twist_.angular.z = 0.0;
+    twist_ = geometry_msgs::msg::Twist();
   }
 
   ~RemapSetpointVelocityPlugin() = default;
 
-  Subscriptions get_subscriptions() override
-  {
-    return { /* Rx disabled */};
+  Subscriptions get_subscriptions() override {
+      return { /* Rx disabled */ };
   }
 
 private:
-  rclcpp::Publisher<geometry_msgs::msg::Twist>::SharedPtr twist_pub_;
-  rclcpp::Subscription<std_msgs::msg::Float32>::SharedPtr vel_x_sub_;
-  rclcpp::Subscription<std_msgs::msg::Float32>::SharedPtr vel_y_sub_;
-  rclcpp::Subscription<std_msgs::msg::Float32>::SharedPtr vel_z_sub_;
-  rclcpp::Subscription<std_msgs::msg::Float32>::SharedPtr vel_r_sub_;
-  
-  geometry_msgs::msg::Twist twist_;
+    rclcpp::Publisher<geometry_msgs::msg::Twist>::SharedPtr twist_pub_;
+    rclcpp::Subscription<std_msgs::msg::Float32>::SharedPtr vel_x_sub_;
+    rclcpp::Subscription<std_msgs::msg::Float32>::SharedPtr vel_y_sub_;
+    rclcpp::Subscription<std_msgs::msg::Float32>::SharedPtr vel_z_sub_;
+    rclcpp::Subscription<std_msgs::msg::Float32>::SharedPtr vel_r_sub_;
 
-  // Timeout management (protected by mutex for thread safety)
-  std::mutex mutex_;
-  rclcpp::Time last_vel_x_time_;
-  rclcpp::Time last_vel_y_time_;
-  rclcpp::Time last_vel_z_time_;
-  rclcpp::Time last_vel_r_time_;
-  rclcpp::TimerBase::SharedPtr publish_timer_;
+    geometry_msgs::msg::Twist twist_;
+    std::mutex mutex_;
+    rclcpp::Time last_vel_x_time_;
+    rclcpp::Time last_vel_y_time_;
+    rclcpp::Time last_vel_z_time_;
+    rclcpp::Time last_vel_r_time_;
+    rclcpp::TimerBase::SharedPtr publish_timer_;
+    double reset_timeout_;
 
-  double reset_timeout_;
+    void publisher_timer_cb() {
+      geometry_msgs::msg::Twist twist_copy;
 
-  /* -*- mid-level helpers -*- */
+      {
+        std::lock_guard<std::mutex> lock(mutex_);
+        rclcpp::Time current_time = node->now();
 
-  bool is_any_active()
-  {
-    rclcpp::Time current_time = node->now();
-    bool any_active = false;
-    
-    if ((current_time - last_vel_x_time_).seconds() > reset_timeout_) {
-      twist_.linear.x = 0.0;
-    } else {
-      any_active = true;
+
+        if ((current_time - last_vel_x_time_).seconds() > reset_timeout_)
+          twist_.linear.x = 0.0;
+
+        if ((current_time - last_vel_y_time_).seconds() > reset_timeout_)
+          twist_.linear.y = 0.0;
+
+        if ((current_time - last_vel_z_time_).seconds() > reset_timeout_)
+          twist_.linear.z = 0.0;
+
+        if ((current_time - last_vel_r_time_).seconds() > reset_timeout_)
+          twist_.angular.z = 0.0;
+
+        twist_copy = twist_;
+      }
+
+      twist_pub_->publish(twist_copy);
     }
-    
-    if ((current_time - last_vel_y_time_).seconds() > reset_timeout_) {
-      twist_.linear.y = 0.0;
-    } else {
-      any_active = true;
-    }
-    
-    if ((current_time - last_vel_z_time_).seconds() > reset_timeout_) {
-      twist_.linear.z = 0.0;
-    } else {
-      any_active = true;
-    }
-    
-    if ((current_time - last_vel_r_time_).seconds() > reset_timeout_) {
-      twist_.angular.z = 0.0;
-    } else {
-      any_active = true;
-    }
-
-    return any_active;
-  }
 };
 
-} // namespace extra_plugins
-} // namespace mavros
+}  // namespace extra_plugins
+}  // namespace mavros
 
 #include <mavros/mavros_plugin_register_macro.hpp>
 MAVROS_PLUGIN_REGISTER(mavros::extra_plugins::RemapSetpointVelocityPlugin)
